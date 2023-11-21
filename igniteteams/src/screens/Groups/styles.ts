cbf9cb225aebaca4a3811fa68c699a2308764d63
H1d3l <hidels.rios@gmail.com>
2023-11-21 17:59:01 -0300

    feat:adicionando theme

---
+++ igniteteams/src/screens/Groups/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+import {useTheme} from 'styled-components';
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: ${() => useTheme().COLORS.GRAY_600};
+
+  justify-content: center;
+  align-items: center;
+`;
