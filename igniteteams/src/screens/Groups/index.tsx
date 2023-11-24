1b0586c9693e2c6021a88ad9a9bbd7a67acfd517
H1d3l <hidels.rios@gmail.com>
2023-11-24 10:00:25 -0300

    feat:listando grupos

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -2,13 +2,21 @@ import { Header } from "@components/Header";
 import { Container } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { GroupCard } from "@components/GroupCard";
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
+
   return (
     <Container>
       <Header />
       <Highlight title="Turmas" subtitle="jogue com sua turma" />
-      <GroupCard title="Nome do Grupo" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
