87a056cd2cf7369cb0dcf962b203f5b17495541d
H1d3l <hidels.rios@gmail.com>
2023-11-24 10:45:50 -0300

    feat:componente list empty

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,10 +1,11 @@
-import { Header } from "@components/Header";
 import { Container } from "./styles";
-import { Highlight } from "@components/Highlight";
-import { GroupCard } from "@components/GroupCard";
 import { useState } from "react";
 import { FlatList } from "react-native";
 
+import { Header } from "@components/Header";
+import { ListEmpty } from "@components/ListEmpty";
+import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 export function Groups() {
   const [groups, setGroups] = useState<string[]>(["Galera da Rocket"]);
 
@@ -16,6 +17,10 @@ export function Groups() {
         data={groups}
         keyExtractor={(item) => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   );
